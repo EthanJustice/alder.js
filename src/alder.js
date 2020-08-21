90323929aea49189c596b84bb26d4f245b4f3a9b
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-08-21 19:43:15 -0400

    chore: switch prettier, linting

--- src/alder.js
+++ src/alder.js
@@ -1,32 +1,35 @@
-"use strict";
+'use strict';
 class Alder {
     constructor(auto = true) {
         this.ids = [];
 
         if (auto == true) {
-            document.body
-                .querySelectorAll("* > style")
-                .forEach(element => this.parse(element.parentElement));
+            document.body.querySelectorAll('* > style').forEach((element) => this.parse(element.parentElement));
         }
     }
 
     parse(element) {
         if (!element) return;
 
-        if (!element.querySelector("style")) return element;
+        if (!element.querySelector('style')) return element;
 
-        let sheet = element.querySelector("style");
+        let sheet = element.querySelector('style');
         let rules = /:|@/;
-        Object.values(sheet.sheet.rules).forEach(item => {
+        Object.values(sheet.sheet.rules).forEach((item) => {
             if (item.selectorText.match(rules) == null) {
-                element.querySelectorAll(`${item.selectorText}`).forEach(child => {
-                    Object.values(item.style).forEach(prop => {
+                element.querySelectorAll(`${item.selectorText}`).forEach((child) => {
+                    Object.values(item.style).forEach((prop) => {
                         child.style[prop] = item.style[prop];
                     });
                 });
-            } else if (item.selectorText.includes(":")) {
+            } else if (item.selectorText.includes(':')) {
                 if (!element.id) element.dataset.alder = this._generate();
-                document.styleSheets[0].insertRule(`${element.id || element.nodeName.toLowerCase()}[data-alder="${element.dataset.alder}"] > ${item.cssText}`, 0);
+                document.styleSheets[0].insertRule(
+                    `${element.id || element.nodeName.toLowerCase()}[data-alder="${element.dataset.alder}"] > ${
+                        item.cssText
+                    }`,
+                    0
+                );
             }
         });
 
@@ -37,8 +40,11 @@ class Alder {
 
     _generate() {
         let i = Math.ceil(Math.random() * 99999);
-        if (this.ids.length == 99999) { return false }
-        else if (this.ids.includes(i)) { i = this._generate() }
+        if (this.ids.length == 99999) {
+            return false;
+        } else if (this.ids.includes(i)) {
+            i = this._generate();
+        }
 
         return i;
     }
